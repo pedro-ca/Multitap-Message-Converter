3df5a0cdf7a4e4d2373b5cb60802bf3374217591
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-05-28 17:59:09 -0300

    Added proper conversion from message to multitap

--- MultitapConverter/MultitapConverter.UnitTest/MultitapEncoderTest.cs
+++ MultitapConverter/MultitapConverter.UnitTest/MultitapEncoderTest.cs
@@ -3,46 +3,51 @@ using MultitapConverter;
 
 namespace MultitapConverter.UnitTest
 {
-    namespace TesteMensagemEscrita
+    public class MultitapEncoderTest
     {
-        public class TesteMensagem
+        [TestCase]
+        public void Mensagem1()
         {
-            [TestCase]
-            public void Mensagem1()
-            {
-                MultitapEncoder encoder = new MultitapEncoder();
-                string message = encoder.EncodeToMultitap("SEMPRE ACESSO O DOJOPUZZLES");
-                
-                Assert.AreEqual("77773367_7773302_222337777_77776660666036"+"6656667889999_9999555337777", message);
-            }
+            MultitapEncoder encoder = new MultitapEncoder();
+            string message = encoder.EncodeToMultitap("SEMPRE ACESSO O DOJOPUZZLES");
 
-            [TestCase]
-            public void Alfabeto()
-            {
-                MultitapEncoder encoder = new MultitapEncoder();
-                string message = encoder.EncodeToMultitap("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-                Assert.AreEqual("2_22_2223_33_3334_44_4445_55_555"+"6_66_6667_77_777_77778_88_8889_99_999_9999", message);
-            }
+            Assert.AreEqual("77773367_7773302_222337777_77776660666036" + "6656667889999_9999555337777", message);
+        }
 
-            [TestCase]
-            public void ErroDigitos()
-            {
-                MultitapEncoder encoder = new MultitapEncoder();
-                string message = encoder.EncodeToMultitap("SEMPRE 12321321");
-                Assert.AreEqual("N„o pode conter dÌgitos", message;
-            }
+        [TestCase]
+        public void Alfabeto()
+        {
+            MultitapEncoder encoder = new MultitapEncoder();
+            string message = encoder.EncodeToMultitap("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            Assert.AreEqual("2_22_2223_33_3334_44_4445_55_555" + "6_66_6667_77_777_77778_88_8889_99_999_9999", message);
+        }
 
-            [TestCase]
-            public void ErroTamanho()
-            {
-                MultitapEncoder encoder = new MultitapEncoder();
-                string message = encoder.EncodeToMultitap("Lorem ipsum dolor sit amet, consectetur " +
-                    "adipiscing elit. Nulla ac urna non diam ullamcorper porta. " +
-                    "Suspendisse a ornare urna, id interdum dui. " +
-                    "Cras vitae elit in diam suscipit fringilla. Pellentesque nec" +
-                    " porttitor augue, ut eleifend erat. Vestibulum fusce.");
-                Assert.AreEqual("N„o pode ser maior que 255", message);
-            }
+        [TestCase]
+        public void ErroDigitos()
+        {
+            MultitapEncoder encoder = new MultitapEncoder();
+            string message = encoder.EncodeToMultitap("SEMPRE 12321321");
+            Assert.AreEqual("Message cannot have any digits.", message);
+        }
+
+        //[TestCase]
+        //public void ErroCaracteresInvalidos()
+        //{
+        //    MultitapEncoder encoder = new MultitapEncoder();
+        //    string message = encoder.EncodeToMultitap("·ÛÈ˙ÍÍÌ·„ıÁ@#$%®&*(");
+        //    Assert.AreEqual("Message cannot non alphabetic characters.", message);
+        //}
+
+        [TestCase]
+        public void ErroTamanho()
+        {
+            MultitapEncoder encoder = new MultitapEncoder();
+            string message = encoder.EncodeToMultitap("Lorem ipsum dolor sit amet, consectetur " +
+                "adipiscing elit. Nulla ac urna non diam ullamcorper porta. " +
+                "Suspendisse a ornare urna, id interdum dui. " +
+                "Cras vitae elit in diam suscipit fringilla. Pellentesque nec" +
+                " porttitor augue, ut eleifend erat. Vestibulum fusce.");
+            Assert.AreEqual("Message cannot have length bigger than 255.", message);
         }
     }
 }
--- MultitapConverter/MultitapConverter/MultitapEncoder.cs
+++ MultitapConverter/MultitapConverter/MultitapEncoder.cs
@@ -5,19 +5,60 @@ using System.Text;
 namespace MultitapConverter
 {
     public class MultitapEncoder
-    { 
+    {
+        Dictionary<string, string> dictionaryKeys;
+
+        public MultitapEncoder()
+        {
+            dictionaryKeys = new Dictionary<string, string>
+            {
+                {"A","2"}, {"B","22"}, {"C","222"}, {"D","3"}, {"E","33" }, {"F","333"},
+                {"G","4"}, {"H","44"}, {"I","444"}, {"J","5"}, {"K","55"}, {"L","555"}, {"M","6"},
+                {"N","66"}, {"O","666"}, {"P","7"}, {"Q","77"}, {"R","777"}, {"S","7777"},
+                {"T","8"}, {"U","88"}, {"V","888"}, {"W","9"}, {"X","99"}, {"Y","999"}, {"Z","9999"},
+                {" ","0"}
+            };
+        }
+
         public string EncodeToMultitap(string message)
         {
+            message = message.ToUpper();
+
             if (message.Length > 255)
-                throw new ArgumentOutOfRangeException("Message cannot have length bigger than 255.");
+                return "Message cannot have length bigger than 255.";
 
+            string result = "";
+            string previousKeyLastDigit = " ";
+            foreach (char c in message)
+            {
+                if (char.IsDigit(c))
+                    return "Message cannot have any digits.";
 
-            
+                if (dictionaryKeys.TryGetValue(c.ToString(), out string value))
+                {
+                    if (value.StartsWith(previousKeyLastDigit))
+                        result += "_";
+
+                    result += value;
+                    previousKeyLastDigit = value.Substring(0,1);
+                }                
+            }
+            return result;
         }
 
         public string DecodeFromMultitap(string multitapCode)
         {
             throw new NotImplementedException();
+            //while (message.Length > 0)
+            //{
+            //    for(int i=3; i>0; i--)
+            //    {
+            //        string firstValues = message.Substring(0, i);
+
+            //        *buscar no dicionario se o valor existe
+            //        *retirar codigo usado da string
+            //    }
+            //}
         }
     }
 }

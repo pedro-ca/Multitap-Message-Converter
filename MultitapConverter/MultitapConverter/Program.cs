110fd15eec331c60c593fd4adf2a130e3d9203c0
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-05-28 16:37:08 -0300

    Initial Commit and .gitignore

---
+++ MultitapConverter/MultitapConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultitapConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
